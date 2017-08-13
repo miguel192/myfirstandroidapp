bbea088ba7f5fe824656da6ccc8c8c6ccde65aba
miguel192 <miguel.angel.g192@gmail.com>
2017-08-13 10:51:47 -0700

    firstcommit

---
+++ app/src/main/java/listview/magc/booton/binne/miguelmyfirstapp/MainActivity.java
@@ -0,0 +1,13 @@
+package listview.magc.booton.binne.miguelmyfirstapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
